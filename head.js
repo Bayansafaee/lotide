76aa0cbb6a636eda4e9c7acc2e06c9af97b360b7
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-25 15:28:27 -0700

    Create a function head which returns the first item in the array.

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = require('./assertEqual');
+
+const head = (arr) => {
+  return arr[0];
+};
+
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
+
+module.exports = head;
