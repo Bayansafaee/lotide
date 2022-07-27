f64daf004cb1a3bad300883981312b17296d770d
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-27 15:05:57 -0700

    Create function which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.

---
+++ findKeyByValue.js
@@ -0,0 +1,20 @@
+// const assertEqual = require("./assertEqual");
+
+const findKeyByValue = (obj, val) => {
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+module.exports = findKeyByValue;
