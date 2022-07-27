d2ba5770b5988683171303f06cc6b7ecd606de60
Bayan Safaee <bayan.safaee@gmail.com>
2022-07-27 15:53:55 -0700

    parameter name update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${[arr1]} === ${[arr2]}`);
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${[actual]} === ${[expected]}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${[arr1]} !== ${[arr2]}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} !== ${[expected]}`);
   }
 };
 
